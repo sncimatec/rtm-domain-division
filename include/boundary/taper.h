6a579bd54c12f15b8cbeef89046097b5966b5a02
Silvano Junior <silvano.junior@fieb.org.br>
2022-05-03 11:57:59 -0300

    Adding tapper2

--- include/boundary/taper.h
+++ include/boundary/taper.h
@@ -4,6 +4,7 @@
 void extendvel(int nx,int nz,int nxb,int nzb,float *vel);
 void taper_init(int nxb,int nzb,float F);
 void taper_apply(float **pp,int nx, int nz, int nxb, int nzb);
+void taper_apply2(float **pp, int nx, int nz, int nxb, int nzb); 
 void taper_destroy();
 
 #endif
